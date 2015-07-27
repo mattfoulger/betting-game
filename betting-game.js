7fd56716754d4eb6044ab273c351da887951b8e4
mattfoulger <mfoulger@gmail.com>
2015-07-27 16:29:18 -0700

    game works in browser with ajax form submission, but no handling of invalid bets or guesses

--- betting-game.js
+++ betting-game.js
@@ -1,4 +1,58 @@
-var bank;
+var bank = 100;
+
+$("#betting-form").submit(function(e) {
+  e.preventDefault();
+  bet = $("#amount").val();
+  guess = $("#guess").val();
+  gamble(bet, guess);
+});
+
+$("#reset-form").submit(function(e) {
+  e.preventDefault();
+  newGame();
+});
+
+
+var gamble = function(bet, guess) {
+  var answer = Math.floor(Math.random() * 10) + 1;
+  var status = "The anwer was " + answer + ". ";
+  if (guess == answer) {
+    bank += (bet * 2);
+    status += "Correct! You won $" + (bet * 2) + "!";
+  } else if (Math.abs(guess - answer) == 1) {
+    status += "You were soooooo close. Keep your bet.";
+  } else {
+    bank -= bet 
+    status += "Better luck next time.";
+  }
+  
+  if (bank < 5) {
+    status = status + " It's GAME OVER, man!";
+    gameOver();
+  }
+
+  $("#status").text(status);
+  $("#bank").text("$"+bank);
+}
+
+var gameOver = function() {
+  $("#betting-form").slideUp(400, function() {
+    $("#reset-form").toggle(200);
+  });
+}
+
+var newGame = function() {
+  $("#reset-form").toggle(100, function() {
+    $("#betting-form").slideDown(400);
+  });
+  bank = 100;
+  $("#status").text("New game... Good luck!");
+  $("#bank").text("$"+bank);
+}
+
+
+
+
 
 
 var gameLoop = function(money) {
@@ -33,26 +87,4 @@ var askForGuess = function() {
   return guess;
 }
 
-var gamble = function(bet, guess) {
-  var answer = Math.floor(Math.random() * 10) + 1;
-  var status = "The anwer was " + answer + ". ";
-  if (guess == answer) {
-    bank += (bet * 2);
-    status += "Correct! You won $" + (bet * 2) + "!";
-  } else if (Math.abs(guess - answer) == 1) {
-    status += "You were soooooo close. Keep your bet.";
-  } else {
-    bank -= bet 
-    status += "Better luck next time.";
-  }
-  
-  if (bank < 5) {
-    status = status + " It's GAME OVER, man!";
-  } else {
-    status = status + " You now have $" + bank + ".";
-  }
 
-  alert(status);
-}
-
-gameLoop(100);
